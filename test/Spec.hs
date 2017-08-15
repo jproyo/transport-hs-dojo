570d52735ba2d31f04bee0ca249cb5064149646c
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-08-15 14:24:50 +0200

    tdd advance

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,7 +10,7 @@ genLNodes = zip [1..5] ["A","B","C","D","E"]
 swapedNodes :: [(String, Int)]
 swapedNodes = map swap genLNodes
 
-lookupNode :: [(String, G.Node)] -> Char -> G.LNode String
+lookupNode :: [(String, G.Node)] -> String -> G.LNode String
 lookupNode pairs ch = case lookup ch pairs of
   Just val -> (val,ch) :: G.LNode String
   Nothing -> (0,"Z") :: G.LNode String
@@ -21,12 +21,18 @@ genLEdges = [(1,2,5),(2,3,4),(3,4,8),(4,3,8),(4,5,6),(1,4,5),(3,5,2),(5,2,3),(1,
 graph :: Gr String Int
 graph = G.mkGraph genLNodes genLEdges
 
-createRouteGraph :: [Char] -> RouteIntoGraph
+splitString :: String -> [String]
+splitString str = map (:[]) str
+
+createRouteGraph :: [String] -> RouteIntoGraph
 createRouteGraph str = mkRouteIntoGraph graph (extractFrom swapedNodes str)
-    where extractFrom lnodes str = LP (map (lookupNode lnodes) str)
+    where extractFrom lnodes str = G.LP (map (lookupNode lnodes) str)
+
+toRoute :: String -> RouteIntoGraph
+toRoute = createRouteGraph . splitString
 
 testCalculateDistance :: String -> Test
-testCalculateDistance route = TestCase (createRouteGraph route @? "Failing")
+testCalculateDistance route = TestCase (calculateDistance (toRoute route) == fromIntegral 1 @? "Failing")
 
 tests :: Test
 tests = TestList [TestLabel "testCalculateDistance A-B-C" (testCalculateDistance "ABC")]
