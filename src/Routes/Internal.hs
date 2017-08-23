ab2615787df0b2ee947cb770c3b6d66f3bd4091f
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-08-23 11:38:07 +0200

    NA

--- src/Routes/Internal.hs
+++ src/Routes/Internal.hs
@@ -1,7 +1,14 @@
 module Routes.Internal (
-pairList,
+pairLPath,
 ) where
 
+import Data.Graph.Inductive.Graph
 
 pairList :: [a] -> [(a,a)]
-pairList = undefined
+pairList [] = []
+pairList [x] = []
+pairList (x:xs) = [(x,head xs)] ++ pairList xs
+
+
+pairLPath :: LPath a -> [(LNode a, LNode a)]
+pairLPath = pairList . unLPath
